886baccac19cf6eddd838a0a66b8e493faa3286e
Yessenin <80644033+kazakhokage@users.noreply.github.com>
2024-01-24 13:14:42 +0600

    Add files via upload

---
+++ advprogcrud/README.md
@@ -0,0 +1,33 @@
+# Zeta Shop
+
+### General Information:
+The purpose of the "Zeta Shop" project is to establish and operate a laptop store that caters to customers in need of high-quality laptops and related accessories. The primary objective is to provide a diverse range of laptops from various brands, ensuring that customers have access to a wide selection of devices to meet their specific requirements.
+
+### Key Features and Functions:
+
+* **Product Catalog:** Zeta Shop will maintain a comprehensive product catalog featuring the latest laptop models, including specifications, features, and pricing details.
+
+* **User-friendly Interface:** The project will incorporate a user-friendly interface for easy navigation, allowing customers to browse through the product catalog, filter options, and find the laptops that best suit their needs.
+
+* **Secure Online Shopping:** Zeta Shop will enable secure online transactions, allowing customers to purchase laptops and accessories with confidence. This involves implementing secure payment gateways and encryption protocols.
+
+* **Order Tracking:** The project will offer order tracking functionality, enabling customers to monitor the status of their purchases from the time of order placement to delivery.
+
+* **Customer Support:** Zeta Shop will provide customer support services to address inquiries, concerns, and provide assistance throughout the shopping process. This may include live chat, email support, and a dedicated customer service hotline.
+
+* **Target Users:**
+The primary target users for Zeta Shop include individuals and businesses in need of laptops and related accessories. This encompasses students, professionals, freelancers, and anyone seeking reliable computing devices. The store aims to appeal to a broad audience by offering a diverse range of products to meet various computing needs.
+
+Overall, the Zeta Shop project aims to create a convenient and reliable platform for purchasing laptops, providing a seamless online shopping experience for its diverse customer base.
+
+![alt Registration page](img.png "Registration page")
+
+### Installation Guide:
+1. Install Golang: https://golang.org/dl/.
+2. Run the server: `go run main.go`.
+3. Use Postman or any HTTP client to send POST requests with JSON data to http://localhost:8080.
+
+### Participants: 
+* Abdukarimov Sultan
+* Baitenov Arsen
+* Kakimov Aldiyar
---
+++ advprogcrud/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	os.Setenv("DATABASE_URL", "user=postgres dbname=advprog password=esengali123 sslmode=disable")
+	var err error
+	db, err = gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&User{}) // Создание таблицы User (если её нет)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to the database")
+
+	//crud
+	db.Create(&User{Username: "esen", Email: "esen@gmail.com", Password: "esen"})
+	// db.Model(&User{}).Where("id = ?", 2).Update("username", "aigali")
+	// db.Unscoped().Delete(&User{}, "id = ?", 3)
+
+	// user, err := getUserByID(4)
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// } else {
+	// 	fmt.Println("User by ID:", user)
+	// }
+
+	// allUsers, err := getAllUsers()
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// } else {
+	// 	fmt.Println("All users:", allUsers)
+	// }
+
+}
+
+type JsonRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Message  string `json:"message"`
+}
+
+type JsonResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+type User struct {
+	gorm.Model
+	Username string
+	Email    string
+	Password string
+}
+
+func main() {
+	http.HandleFunc("/", handleRequest)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// Добавляем поддержку CORS для запросов OPTIONS
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodPost:
+		handlePostRequest(w, r)
+	case http.MethodGet:
+		handleGetRequest(w, r)
+	default:
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	var requestData JsonRequest
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestData)
+	if err != nil {
+		http.Error(w, `{"status": "400", "message": "Invalid JSON message"}`, http.StatusBadRequest)
+		return
+	}
+
+	if requestData.Message == "" && (requestData.Username == "" || requestData.Email == "" || requestData.Password == "") {
+		http.Error(w, `{"status": "400", "message": "Either 'message' or all 'username', 'email', 'password' fields must be provided"}`, http.StatusBadRequest)
+		return
+	}
+
+	if requestData.Message != "" {
+		fmt.Println("Received POST message:", requestData.Message)
+	} else {
+		fmt.Printf("Received POST request:\nUsername: %s\nEmail: %s\nPassword: %s\n --------------\n", requestData.Username, requestData.Email, requestData.Password)
+	}
+
+	saveError := saveRegistrationData(requestData.Username, requestData.Email, requestData.Password)
+	if saveError != nil {
+		http.Error(w, `{"status": "500", "message": "Failed to save registration data"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	response := JsonResponse{
+		Status:  "success",
+		Message: "Data successfully received",
+	}
+
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJSON)
+}
+
+func handleGetRequest(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+
+	name := queryParams.Get("name")
+	age := queryParams.Get("age")
+
+	if name == "" || age == "" {
+		http.Error(w, `{"status": "400", "message": "Both name and age parameters are required in the GET request"}`, http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Received GET request with name: %s, age: %s\n", name, age)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	response := JsonResponse{
+		Status:  "success",
+		Message: fmt.Sprintf("Name: %s, Age: %s", name, age),
+	}
+
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJSON)
+}
+
+func saveRegistrationData(username, email, password string) error {
+	user := User{Username: username, Email: email, Password: password}
+	result := db.Create(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func getUserByID(userID uint) (User, error) {
+	var user User
+	result := db.Select("id, username, email, password").First(&user, userID)
+	if result.Error != nil {
+		return User{}, result.Error
+	}
+	return user, nil
+}
+
+func getAllUsers() ([]User, error) {
+	var users []User
+	result := db.Select("id, username, email, password").Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return users, nil
+}
